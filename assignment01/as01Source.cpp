2db97d3f60568adccd80f57f444170b8185c5fc3
Kim <nguyennt44@vcu.edu>
2018-09-20 21:10:33 -0400

    Deleted old code. Added program requirement and author's name and date

--- assignment01/as01Source.cpp
+++ assignment01/as01Source.cpp
@@ -1,57 +1,44 @@
 /*
-#include <iostream>
-using namespace std;
+Topics Covered:  console input & output,  loops  and  conditional statements
 
-//Program to determines number of bills to return with a withdrawal request 
+Functional Requirements:
+You work for a progressive bank that is installing ATMs which will deliver withdrawals in  multiple bill denominations: $50, $20, $10, $5 and $1
+Write a program that determines number of bills to return with a withdrawal request.
+Your program should start by  requesting the user to enter the amount to withdrawn
+The amount entered should be validated
+Withdraws must be > $1.00
+Withdrawals cannot be > $300.00
+If the  Amount is not valid then the user will be asked to re-enter until a valid number is entered.
+After a valid number is entered  the # of each bill type should be determined and presented, optimizing for the fewest number of bills
+E.g.  Withdrawal of $200 should be 4 x $50.00  (versus 20  x $10 bills)
+#  $50.00 bills
+# $20.00 bills
+# $10.00 bills
+# $5.00 bills
+# $1.00 bills
+Only show the amount if the number of bills is  >  0
+e.g   do not show :   0  $50.00 bills
+After presenting the change, the program should allow the use to enter another transaction
+The program should continue accepting input and making change until the user enters  ‘0’ to quit.
 
-int main()
-{
-	//local variable declariation
-	int withdrawalAmount;
-	int i = 0;
-	int billValue_50 = 50;
-	int billValue_20 = 20;
-	int billValue_10 = 10;
-	int billValue_5 = 5;
-	int billValue_1 = 1;
+_____________________
 
-	//FRIST do-while Loop condition making sure a valid input is entered
-	do {
-		//Empty line
-		cout << " " << endl;
+Non Functional Requirements
+Your program should be properly commented, clearly indicating what the program is doing
+Your program should employ the use at least 2 loops (you choose: for or while or do-while)
+	one that loops until a valid input is entered
+	one that loops to allow for additional withdrawals
+DO not use GOTOs
 
-		//Ask user to enter a withdrawal amount
-		cout << " Enter an ammount you wish to withdraw" << endl;
-		cout << " Enter 0 to quit" << endl;
-		cin >> withdrawalAmount;
+Submission Requirements
+You  should upload your program to your github account  in a repository called:  INFO450ATM
+Turn in your project on blackboard by uploading the URL to your github repository
+*/
 
-		//Separating the amount entered from the number of bills with an empty line
-		cout << " " << endl;
-
-		//Execution STOPS when user hit 0
-		if (withdrawalAmount == 0)
-			break;
-
-		//SECOND (inner) for Loop condition allowing user to enter another transation 'till user enter a 0
-		for (i = withdrawalAmount - 1; i >= 1; i--)
-		{
-			if (withdrawalAmount % i == 0)
-			{
-				//Determining # of bills to return 
-
-			}
-		}
-		if (i == 1)
-			return 0;
-
-
-	} while (withdrawalAmount > i);
-
-	cin.get();
-	//cin.get();
-	return 0;
-
-}
+/*	
+	Assignment 01
+	Author: Kim Nguyen 
+	Date: Thursday, September 20, 2018  
 */
 
 #include <iostream>
