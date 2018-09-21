55298b6da6e5a3a326c0a6d718ca9de8e90f1013
Kim <nguyennt44@vcu.edu>
2018-09-20 20:48:20 -0400

    modified new code and commented the previous code

--- assignment01/as01Source.cpp
+++ assignment01/as01Source.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 using namespace std;
 
@@ -50,4 +51,105 @@ int main()
 	//cin.get();
 	return 0;
 
+}
+*/
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	char answer = 'Y';
+	double withdraw = 0;
+	int balance, Fifties, Twenties, Tens, Fives, Ones;
+
+	cout << "" << endl;
+	cout << " Hello valued customer!" << endl << endl;
+	cout << " Are you looking to withdraw from this ATM machine? Let me help!" << endl;
+	cout << "" << endl;
+	cout << " ***** Please remember: This ATM machine can only handle withdrawl of at least $1 and no more than $300 ***** " << endl;
+	cout << "" << endl;
+	cout << " ======================================================== " << endl;
+	cout << "" << endl;
+
+	// Initiates a while loop to verify user selection to withdraw more funds after the initial withdrawal.
+	while (answer == 'Y' || answer == 'y')			
+	{
+		do
+		{
+			cout << " Please enter the amount you wish to withdraw: $";
+			cin >> withdraw;
+			cout << "" << endl;
+
+			if (withdraw <= 300 && withdraw >= 1) // Checks whether the user's value to withdraw is within the allowed withdrawl range
+			{
+				cout << " You've chosen to withdraw $" << withdraw << endl;
+				cout << "" << endl;
+
+				// Calculates the amount of dollars dispense based on user's input
+				Fifties = withdraw / 50;		  
+				balance = withdraw - (Fifties * 50);
+				Twenties = balance / 20;
+				balance = balance - (Twenties * 20);
+				Tens = balance / 10;
+				balance = balance - (Tens * 10);
+				Fives = balance / 5;
+				balance = balance - (Fives * 5);
+				Ones = balance / 1;
+				balance = balance - (Ones * 1);
+
+				cout << " Your withdrawl then breaks down to: " << endl << endl;;
+				cout << "" << endl;
+				cout << "\t =======================";
+				cout << "" << endl;
+
+				// nested if condition to cycle through each type of bill and returns the amount of bills if the value exceeds $0
+				if (Fifties >= 1)				
+				{
+					cout << "\n\t You have " << Fifties << " $50 bills: " << endl;
+				}
+
+				if (Twenties >= 1)
+				{
+					cout << "\n\t You have " << Twenties << " $20 bills: " << endl;
+				}
+
+				if (Tens >= 1)
+				{
+					cout << "\n\t You have " << Tens << " $10 bills: " << endl;
+				}
+
+				if (Fives >= 1)
+				{
+					cout << "\n\t You have " << Fives << " $5 bills: " << endl;
+				}
+
+				if (Ones >= 1)
+				{
+					cout << "\n\t You have " << Ones << " $1 bills: " << endl;
+				}
+				cout << "\n\t =======================" << endl;
+			}
+			else
+			{
+				cout << " Invalid amount entered.  Remember? The minimum withdraw is $1.00 & the maximum is $300.00.  Please try again :) " << endl;
+				cout << "" << endl;
+			}
+		}
+		while (withdraw == 0);
+
+		cout << endl;
+		cout << " Would you like to perform another withdrawal? Enter Y to continue, or enter N to end ---> ";	// Asks the user to make a selection on whether to do another transaction
+		cin >> answer;
+		cout << "" << endl;
+		cout << " ========================================================" << endl;
+		cout << "" << endl;
+	}
+
+	// Exits the program if 'n' or 'N' is entered
+	cout << " Ending your transaction!  Thank you for using the service ^.^" << endl;			
+	cout << endl;
+	system("pause");
+	return 0;
 }
